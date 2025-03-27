10ccc1f827bb9887663906715709197eaeaa9720
Dirk <d.vanroosmalen@student.han.nl>
2025-03-27 20:36:24 +0100

    Initial commit

---
+++ commands.js
@@ -0,0 +1,15 @@
+import 'dotenv/config';
+import { InstallGlobalCommands } from './utils.js';
+
+// Simple test command
+const STATS_COMMAND = {
+  name: 'stats',
+  description: 'Basic command',
+  type: 1,
+  integration_types: [0],
+  contexts: [0],
+};
+
+const ALL_COMMANDS = [STATS_COMMAND];
+
+InstallGlobalCommands(process.env.APP_ID, ALL_COMMANDS);
---
+++ messageListener.js
@@ -0,0 +1,28 @@
+import pkg from "discord.js";
+import "dotenv/config";
+const { Client, GatewayIntentBits } = pkg;
+
+// const channelId = process.env.CHANNEL_ID;
+const channelId = "1354219423178821692";
+
+const discordClient = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent],
+});
+
+discordClient.once("ready", (c) => {
+  console.log(`Logged in as ${c.user.tag}`);
+});
+
+discordClient.on("messageCreate", async (messageObject) => {
+  if (messageObject.author.bot) return;
+  if (messageObject.channel.id !== channelId) return;
+
+  let mentionedUsers = messageObject.mentions.users;
+  console.log(mentionedUsers);
+
+  await messageObject.react("📣");
+});
+
+export async function startChannelListener() {
+  discordClient.login(process.env.DISCORD_BOT_TOKEN);
+}
